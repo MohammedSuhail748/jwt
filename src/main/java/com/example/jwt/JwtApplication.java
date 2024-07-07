edbcbd5b1d843306d52f24a204ffe33a4eec588e
MohammedSuhail786 <mdsuhail01899@gmail.com>
2024-07-07 12:31:42 +0530

    added hello world

---
+++ src/main/java/com/example/jwt/JwtApplication.java
@@ -0,0 +1,16 @@
+package com.example.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtApplication.class, args);
+		System.out.println("Hello world");
+
+	}
+
+}
